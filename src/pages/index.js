c55855148682e3abf41faa04e78a744bf42b64c0
Andrea Salvatori <andrea.salvatori92@gmail.com>
2020-11-06 00:30:20 +0100

    Fix site title

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ const IndexPage = ({ data }) => {
   const files = data.file
   return (
     <Layout>
-      <SEO title="Home" />
+      <SEO labg="en" title="Home" />
       <About
         shortName={page.shortName}
         authorName={page.authorName}
